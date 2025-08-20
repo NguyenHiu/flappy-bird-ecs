5a4f55817267d428d56c0a6246d9a39c08c683b3
Gavin <nguyenhieu82132@gmail.com>
2025-08-21 01:02:13 +0700

    chore: remove build folder

--- build/game_manager.d
+++
@@ -1,8 +0,0 @@
-build/game_manager.o: src/game_manager.cpp include/game_manager.h \
-  include/abstract_system.h include/entity.h include/utils.h \
-  include/logger.h
-include/game_manager.h:
-include/abstract_system.h:
-include/entity.h:
-include/utils.h:
-include/logger.h:
